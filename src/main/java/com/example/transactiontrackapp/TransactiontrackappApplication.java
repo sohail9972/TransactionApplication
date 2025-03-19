fd596da760c34826a95ed30ce2fe50b8d2ec8509
sohail9972 <2018ece_sohailmaidargi@nie.ac.in>
2025-03-19 23:36:26 +0530

    adding all files

---
+++ src/main/java/com/example/transactiontrackapp/TransactiontrackappApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactiontrackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactiontrackappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactiontrackappApplication.class, args);
+	}
+
+}
