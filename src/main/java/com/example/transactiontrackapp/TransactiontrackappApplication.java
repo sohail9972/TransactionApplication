d523d78bd69166fae3add76351d297a816dcf9f8
sohail9972 <2018ece_sohailmaidargi@nie.ac.in>
2025-03-26 23:59:22 +0530

    adding dummy text

--- src/main/java/com/example/transactiontrackapp/TransactiontrackappApplication.java
+++ src/main/java/com/example/transactiontrackapp/TransactiontrackappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TransactiontrackappApplication {
 
-//	checking the Test solo dfkdf
+//	checking the Test solo main Application
 	public static void main(String[] args) {
 		SpringApplication.run(TransactiontrackappApplication.class, args);
 	}
